a1fe2db72683fcf72785f2780c4bde8aebf4c816
kafdel11 <kafdeldesmond@gmail.com>
2024-04-01 02:28:59 +0200

    nouvel update

--- mes articles/test.py
+++ mes articles/test.py
@@ -1,3 +1,7 @@
-# Exemple d'utilisation de la boucle for
-nombre = {"nom": "afdel", "prenom":"desmond", "nom":"edite"}
-print(nombre)
+# Création d'un dictionnaire avec des clés uniques
+mon_dictionnaire = {'a': 1, 'b': 2, 'c': 3}
+
+# Tentative d'ajouter une clé identique
+mon_dictionnaire['a'] = 4  # Cette opération met à jour la valeur associée à la clé 'a'
+
+print(mon_dictionnaire) # Affiche {'a': 4, 'b': 2, 'c': 3}
